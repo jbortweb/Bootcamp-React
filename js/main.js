5dacae86187370167cd8ecb5e47a22e71f1f6ee8
jbortweb <jbortweb@gmail.com>
2022-04-26 15:59:48 +0200

    Kata8 Metodos Array

--- js/main.js
+++ js/main.js
@@ -1,28 +1,55 @@
-function suma(num1, num2){
+//Arrays
+/*        
+var notas = [5, 6, 7, 8, 3, 2, 4, 10, 3];
 
-    return num1 + num2;
+console.log (notas[2]); // 7
+console.log (notas.length)  //9
+
+notas[2] = 5;
+console.log (notas[2]); // 5
+
+for (let i = 0; i < notas.length; i++){
+
+    console.log ('Bucle nº '+ i);
+    console.log (notas[i]);
 }
+*/
+//Metodo Push
 
-console.log (suma(4,6));
+var notas = [];
+var notasClaseB = [3, 5, 8];
+var nAlumnos = 5;
 
-
-
-        // Funcion anonima
-
-var elevar = function(num,exp){
-    return num**exp;
+for (var i = 1; i <= nAlumnos; i++) {
+  var newNota = prompt("Introduce la nota nº " + i);
+  notas.push(Number(newNota));
 }
-console.log (elevar(3,3));
+console.log('Metodo push, añade al final');
+console.log(notas);
 
+//Metodo Concat
+console.log('Metodo contac (3, 5, 8)');
+console.log(notas.concat(notasClaseB));
 
+//Metodo Join
+console.log('Metodo join, string separado por puntos');
+console.log(notas.join('.'));
 
-        //Dibujar tabla multiplicar
+//Metodo unshift
+console.log('Metodo unshift, coloca delante del array');
+console.log(notas.unshift(2));
+console.log(notas);
 
-function DibujarTabla(table){
+//Metodo shift
+console.log('Metodo shift, elimina primer elemento');
+console.log(notas.shift());
+console.log(notas);
 
-    for( let i = 0; i <= 10; i++){
-        console.log (table + ' X ' + i + ' = ' + (table*i));
-    }
-}
+//Metodo pop
+console.log('Metodo pop, elimina ultimo elemento');
+console.log(notas.pop());
+console.log(notas);
 
-DibujarTabla(3);
+//Metodo Reverse
+console.log('Metodo reverse');
+console.log(notas.reverse());
