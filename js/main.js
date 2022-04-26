53f7ee1c65833d350f34510a78adfaa409a799fa
jbortweb <jbortweb@gmail.com>
2022-04-26 11:05:36 +0200

    Operador ternario

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var edad = 18;
+/*
+var puedoVerPelicula;
+
+if(edad >= 18){
+    puedoVerPelicula ='SI';
+}else{
+    puedoVerPelicula ='NO';
+}
+console.log (puedoVerPelicula);
+
+*/
+
+// Operador ternario
+
+var puedoVerPelicula = (edad >= 18)
+    ? 'SI'  // Se cumple la condicion
+    : 'NO'  // No se cumple la condicion
