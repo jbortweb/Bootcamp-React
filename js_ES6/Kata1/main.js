b983a4b6651e4073e2dfff35295a611bcac278af
jbortweb <jbortweb@gmail.com>
2022-04-27 11:33:21 +0200

    Calcular area y perimetro

--- js_ES6/Kata1/main.js
+++ js_ES6/Kata1/main.js
@@ -1,4 +1,11 @@
-let radio = Number(prompt('Indica la medida del radio'));
+/* let radio = Number(prompt('Indica la medida del radio'));
 const pi = 3.14;
 
 alert ('El perimetro del círculo es ' + (2*pi*radio) + '. El área del círculo es ' + (pi*(radio**2)));
+*/
+function calcular(){
+let radio = document.getElementById('cifra').value;
+const pi = 3.14;
+
+document.getElementById("parrafo").innerHTML = ('El perimetro del círculo es ' + (2*pi*radio).toFixed(2) + '. El área del círculo es ' + (pi*(radio**2)).toFixed(2));
+}
