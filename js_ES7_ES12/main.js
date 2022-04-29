c8e527c16da021731709507ef247f1cc80b05fa1
jbortweb <jbortweb@gmail.com>
2022-04-29 10:59:30 +0200

    Object entries y values

---
+++ js_ES7_ES12/main.js
@@ -0,0 +1,16 @@
+var obj = {
+  test: 1,
+  foo: true,
+  string: "nombre",
+};
+
+console.log(Object.entries(obj));
+
+var obj = {
+  test: 1,
+  foo: true,
+  string: "nombre",
+};
+
+console.log(Object.values(obj));
+
